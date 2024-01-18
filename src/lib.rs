6d310814c7f6d0cefbcb1aa70e53d0a2890a8c21
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-01-17 19:45:43 -0800

    Save changes while going through a tutorial

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,16 +16,102 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
 
+/*
+  From rust doc including this example...
+  https://doc.rust-lang.org/book/ch12-04-testing-the-librarys-functionality.html
+
+  Notice that we need to define an explicit lifetime 'a in the signature of
+  search and use that lifetime with the contents argument and the return value.
+  Recall in Chapter 10 that the lifetime parameters specify which argument
+  lifetime is connected to the lifetime of the return value. In this case, we
+  indicate that the returned vector should contain string slices that reference
+  slices of the argument contents (rather than the argument query).
+  In other words, we tell Rust that the data returned by the search function
+  will live as long as the data passed into the search function in the contents
+  argument. This is important! The data referenced by a slice needs to be valid
+  for the reference to be valid; if the compiler assumes we’re making string
+  slices of query rather than contents, it will do its safety checking
+  incorrectly.
+*/
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
