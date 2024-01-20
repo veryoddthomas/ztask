43877c3de31c29a2684105849da7e4a189b7e1a6
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-01-20 15:47:41 -0800

    Add TaskList and remove hacky initialization

--- src/lib.rs
+++ src/lib.rs
@@ -47,12 +47,22 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Note: write back will restrict to known data structure
     // for Task, and remove other fields.
     // let tasks = task::read_db().expect("fetch task list");
-    let tasks = task::read_db().expect("fetch task list");
-    for t in &tasks {
-        println!("{name}", name = t.name);
-    }
 
-    task::write_db(&tasks);
+    // let mut tasks = task::read_db().expect("fetch task list");
+    // for t in &tasks {
+    //     println!("{name}", name = t.name);
+    // }
+    // let new_task = task::Task::new("Write Rust code".to_string(), "work".to_string());
+    // tasks.push(new_task);
+    // task::write_db(&tasks);
+    // task::TaskList::new_with_tasks(tasks).print_list();
+
+    let mut task_list = task::TaskList::new();
+    task_list.print_list();
+
+    let new_task = task::Task::new("New task".to_string(), "".to_string());
+    task_list.add_task(new_task);
+    task_list.print_list();
 
     Ok(())
 }
--- src/task.rs
+++ src/task.rs
@@ -6,6 +6,7 @@ use std::fs;
 use std::io;
 use thiserror::Error;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 const DB_PATH: &str = "./data/db.json";
 
@@ -23,63 +24,232 @@ pub enum Error {
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Task {
-    pub id: usize,
+    pub id: Uuid,
     pub name: String,
     pub category: String,
     pub created_at: DateTime<Utc>,
 }
 
-pub fn read_db() -> Result<Vec<Task>, Error> {
-    let db_content = fs::read_to_string(DB_PATH)?;
-    let parsed: Vec<Task> = serde_json::from_str(&db_content)?;
-    Ok(parsed)
+impl Task {
+    pub fn new(name: String, category: String) -> Self {
+        Task {
+            id: Uuid::new_v4(),
+            name,
+            category,
+            created_at: Utc::now(),
+        }
+    }
 }
 
-/// Write tasks to DB (json file)
-pub fn write_db(tasks: &Vec<Task>) {
-    let json = serde_json::to_string(tasks).unwrap();
+pub struct TaskList {
+    pub tasks: Vec<Task>,
+}
 
-    let mut file = File::create(DB_PATH).unwrap();
-    file.write_all(json.as_bytes()).unwrap();
+impl Drop for TaskList {
+    fn drop(&mut self) {
+        // println!("Destroy TaskList");
+        self.save().unwrap();
+        self.tasks.clear();
+    }
+}
+
+impl TaskList {
+    pub fn new() -> Self {
+        TaskList::load().unwrap()
+    }
+
+    /// Create new task list with given tasks.
+    /// WARNING: This will overwrite the existing tasks in the DB file.
+    pub fn new_with_tasks(tasks: Vec<Task>) -> Self {
+        TaskList { tasks }
+    }
+
+    pub fn print_list(&self) {
+        for task in &self.tasks {
+            println!("Task: {} {}", task.id, task.name);
+        }
+    }
+
+    pub fn save(&self) -> Result<(), Error> {
+        let serialized = serde_json::to_string_pretty(&self.tasks)?;
+        let mut file = File::create(DB_PATH)?;
+        file.write_all(serialized.as_bytes())?;
+        Ok(())
+    }
+
+    pub fn load() -> Result<TaskList, Error> {
+        let contents = fs::read_to_string(DB_PATH)?;
+        let tasks: Vec<Task> = serde_json::from_str(&contents)?;
+        Ok(TaskList::new_with_tasks(tasks))
+    }
+
+    // pub fn save(&self) -> Result<(), Error> {
+    //     let serialized = serde_json::to_string_pretty(&self.tasks)?;
+    //     let mut file = File::create(DB_PATH)?;
+    //     file.write_all(serialized.as_bytes())?;
+    //     Ok(())
+    // }
+
+    // pub fn load() -> Result<TaskList, Error> {
+    //     let contents = fs::read_to_string(DB_PATH)?;
+    //     let tasks: Vec<Task> = serde_json::from_str(&contents)?;
+    //     Ok(TaskList::new_with_tasks(tasks))
+    // }
+
+    pub fn add_task(&mut self, task: Task) {
+        self.tasks.push(task)
+    }
+
+    // pub fn remove_task(&mut self, id: Uuid) {
+    //     self.tasks.retain(|task| task.id!= id)
+    // }
+
+    // pub fn get_task(&self, id: Uuid) -> Option<&Task> {
+    //     self.tasks.iter().find(|task| task.id == id)
+    // }
+
+    // pub fn get_tasks_by_category(&self, category: &str) -> Vec<&Task> {
+    //     self.tasks.iter().filter(|task| task.category == category).collect()
+    // }
+
+    // pub fn get_tasks_by_name(&self, name: &str) -> Vec<&Task> {
+    //     self.tasks.iter().filter(|task| task.name == name).collect()
+    // }
+
+    // pub fn get_tasks_by_name_and_category(&self, name: &str, category: &str) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.name == name && task.category == category)
+    //        .collect()
+    // }
+
+    // pub fn get_tasks_by_category_and_name(&self, name: &str, category: &str) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.name == name && task.category == category)
+    //        .collect()
+    // }
+
+    // pub fn get_tasks_by_name_and_category_and_date(
+    //     &self,
+    //     name: &str,
+    //     category: &str,
+    //     date: &DateTime<Utc>,
+    // ) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.name == name && task.category == category && task.created_at == date)
+    //        .collect()
+    // }
+
+    // pub fn get_tasks_by_category_and_name_and_date(
+    //     &self,
+    //     name: &str,
+    //     category: &str,
+    //     date: &DateTime<Utc>,
+    // ) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.name == name && task.category == category && task.created_at == date)
+    //        .collect()
+    // }
+
+    // pub fn get_tasks_by_name_and_date(&self, name: &str, date: &DateTime<Utc>) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.name == name && task.created_at == date)
+    //        .collect()
+    // }
+
+    // pub fn get_tasks_by_category_and_date(&self, category: &str, date: &DateTime<Utc>) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.category == category && task.created_at == date)
+    //        .collect()
+    // }
+
+    // pub fn get_tasks_by_date(&self, date: &DateTime<Utc>) -> Vec<&Task> {
+    //     self.tasks.iter().filter(|task| task.created_at == date).collect()
+    // }
+
+    // pub fn get_tasks_by_name_and_category_and_date_range(
+    //     &self,
+    //     name: &str,
+    //     category: &str,
+    //     start_date: &DateTime<Utc>,
+    //     end_date: &DateTime<Utc>,
+    // ) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.name == name && task.category == category && task.created_at >= start_date && task.created_at <= end_date)
+    //        .collect()
+    // }
+
+    // pub fn get_tasks_by_category_and_name_and_date_range(
+    //     &self,
+    //     name: &str,
+    //     category: &str,
+    //     start_date: &DateTime<Utc>,
+    //     end_date: &DateTime<Utc>,
+    // ) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.name == name && task.category == category && task.created_at >= start_date && task.created_at <= end_date)
+    //        .collect()
+    // }
+
+    // pub fn get_tasks_by_name_and_date_range(&self, name: &str, start_date: &DateTime<Utc>, end_date: &DateTime<Utc>) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.name == name && task.created_at >= start_date && task.created_at <= end_date)
+    //        .collect()
+    // }
+
+    // pub fn get_tasks_by_category_and_date_range(&self, category: &str, start_date: &DateTime<Utc>, end_date: &DateTime<Utc>) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.category == category && task.created_at >= start_date && task.created_at <= end_date)
+    //        .collect()
+    // }
+
+    // pub fn get_tasks_by_date_range(&self, start_date: &DateTime<Utc>, end_date: &DateTime<Utc>) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.created_at >= start_date && task.created_at <= end_date)
+    //        .collect()
+    // }
+
+    // pub fn get_tasks_by_name_and_category_and_date_range_and_time_range(
+    //     &self,
+    //     name: &str,
+    //     category: &str,
+    //     start_date: &DateTime<Utc>,
+    //     end_date: &DateTime<Utc>,
+    //     start_time: &DateTime<Utc>,
+    //     end_time: &DateTime<Utc>,
+    // ) -> Vec<&Task> {
+    //     self.tasks
+    //        .iter()
+    //        .filter(|task| task.name == name && task.category == category && task.created_at >= start_date && task.created_at <= end_date && task.created_at >= start_time && task.created_at <= end_time)
+    //        .collect()
+    // }
 }
 
 
-// xref: /usr/local/develop/rust-commandline-example/src/main.rs
 
-// fn add_task_to_db() -> Result<Vec<Task>, Error> {
-//     let mut rng = rand::thread_rng();
+// pub fn read_db() -> Result<Vec<Task>, Error> {
 //     let db_content = fs::read_to_string(DB_PATH)?;
-//     let mut parsed: Vec<Task> = serde_json::from_str(&db_content)?;
-//     let catsdogs = match rng.gen_range(0, 1) {
-//         0 => "cats",
-//         _ => "dogs",
-//     };
-
-//     let random_pet = Task {
-//         id: rng.gen_range(0, 9999999),
-//         name: rng.sample_iter(Alphanumeric).take(10).collect(),
-//         category: catsdogs.to_owned(),
-//         age: rng.gen_range(1, 15),
-//         created_at: Utc::now(),
-//     };
-
-//     parsed.push(random_pet);
-//     fs::write(DB_PATH, &serde_json::to_vec(&parsed)?)?;
+//     let parsed: Vec<Task> = serde_json::from_str(&db_content)?;
 //     Ok(parsed)
 // }
 
-// fn remove_pet_at_index(pet_list_state: &mut ListState) -> Result<(), Error> {
-//     if let Some(selected) = pet_list_state.selected() {
-//         let db_content = fs::read_to_string(DB_PATH)?;
-//         let mut parsed: Vec<Pet> = serde_json::from_str(&db_content)?;
-//         parsed.remove(selected);
-//         fs::write(DB_PATH, &serde_json::to_vec(&parsed)?)?;
-//         let amount_pets = read_db().expect("can fetch pet list").len();
-//         if selected > 0 {
-//             pet_list_state.select(Some(selected - 1));
-//         } else {
-//             pet_list_state.select(Some(0));
-//         }
-//     }
-//     Ok(())
+// /// Write tasks to DB (json file)
+// pub fn write_db(tasks: &Vec<Task>) {
+//     let json = serde_json::to_string(tasks).unwrap();
+
+//     let mut file = File::create(DB_PATH).unwrap();
+//     file.write_all(json.as_bytes()).unwrap();
 // }
+
+// xref: /usr/local/develop/rust-commandline-example/src/main.rs
+
